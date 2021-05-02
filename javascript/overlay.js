2c0948e4de978d5c50c1083b80fe9a5e6d2b679d
Lukáš Urban <lukas.urbann@seznam.cz>
2021-05-03 00:44:30 +0200

    Website is ready

--- javascript/overlay.js
+++ javascript/overlay.js
@@ -1,16 +1,19 @@
 var notification_popup = document.getElementById("notification");
+var notification_content = document.getElementsByClassName("notification-content");
 var notification_text = document.getElementById("notification-text");
 var span = document.getElementById("notification-close");
 
-function show_notification (type)
+function show_notification(type)
 {
-  if(arguments == 0)
+  if(typeof type === "undefined")
   {
-
+    notification_popup.style.display = "block";
+    notification_content[0].style.display = "none";
   }
   else
   {
     notification_popup.style.display = "block";
+    notification_text.innerHTML = type;
   }
 }
 
