2cc21d16438a6c95c11917f125cc7052fd6b60ce
Lukáš Urban <lukas.urbann@seznam.cz>
2021-07-08 14:54:40 +0200

    complete project grid boxes

--- javascript/interface-language.js
+++ javascript/interface-language.js
@@ -14,15 +14,15 @@ writeLang("nav-studium").innerHTML = "Studium";
 writeLang("nav-contact").innerHTML = "Kontakt";
 writeLang("nav-about").innerHTML = "O mně";
 //DESCRIPTIONS
-writeLang("desc-mathack").innerHTML = "Jednoduchý program - slouží jako vzdělávací pomůcka matematiky. Program cílen pro širokou veřejnost.";
-writeLang("desc-moralityabyss").innerHTML = "Hra vytvořena pro školní projekt, v projektu byla použita grafika podobná starým hrám z dob PS1.";
-writeLang("desc-spaceinvaders").innerHTML = "Projekt vytvořen jako zadání pro klauzurní práce. Snaha zreplikovat a modernizovat klasickou hru Space Invaders.";
+desc-mathack
+desc-moralityabyss
+desc-spaceinvaders
 //TEXTS
-writeLang("studium-text").innerHTML = "Již třetím rokem studuji na střední škole ve Zlíně obor vývoj počítačových her a multimediálních aplikací. Jsem zaměřen převážně na tvorbu desktopových aplikací a vývoj indie video her. Pracuji primárně v jazycích jako Java, Python nebo C++.";
-writeLang("about-text").innerHTML = "Jmenuji se Lukáš Urban a profesně se zabývám programováním a výpočetní technologií. Je to mou vášní již od dětství, kdy jsem tvořil své první prototypy her v GameMakeru. Tvořím moderní, robustní, ale hlavně srozumitelné a snadno použivatelné aplikace.";
-writeLang("contact-text").innerHTML = "Nejrychlejší forma kontaktu je zkrze email nebo discord. Po domluvě jsem ochoten přistoupit i na další formy komunikace";
+studium-text
+about-text
+contact-text
 //FOOTER
-writeLang("footer-my-profiles").innerHTML = "- Mé profily -";
+footer-my-profiles
 
 //Lang//Lang//Lang//Lang//Lang//Lang
 
