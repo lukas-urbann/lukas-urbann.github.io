b1501334d8018eca7d0b9af877c8571cf6599c98
Lukáš Urban <lukas.urbann@seznam.cz>
2024-10-17 22:50:39 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'dark-white': '#cccccc',
+        'normal-red': '#f73d3d',
+        'normal-white': '#ffffff',
+        'dark-black': '#0f1119',
+        'normal-black': '#1d1f2b',
+      },
+    },
   },
   plugins: [],
 }
